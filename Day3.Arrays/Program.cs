00262ca871a84d0c02afa61d0186e40d344806de
Aswathy Krishnan <aswathy@Aswathys-MacBook-Air.local>
2024-10-03 17:44:25 +0530

    day3commit

--- Day3.Arrays/Program.cs
+++ Day3.Arrays/Program.cs
@@ -10,27 +10,135 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            int numbers = new int[5];
-            Console.Writeline("Enter 5 numbers : ");
-            int sum = 0;
+            // int numbers = new int[5];
+            // Console.Writeline("Enter 5 numbers : ");
+            // int sum = 0;
 
-            for (int i = 0; i < 5; i++)
-            {
-                numbers[i] = int.Parse(Console.ReadLine());
-            }
+            // for (int i = 0; i < 5; i++)
+            // {
+            //     numbers[i] = int.Parse(Console.ReadLine());
+            // }
 
-            System.Console.WriteLine("The numbers multiplied by 2 : ");
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(numbers[i] * 2);
-            }
+            // System.Console.WriteLine("The numbers multiplied by 2 : ");
+            // for (int i = 0; i < 5; i++)
+            // {
+            //     Console.WriteLine(numbers[i] * 2);
+            // }
 
-            System.Console.WriteLine("The sum of the even numbers : ");
-            if(numbers[i] % 2 == 0)
-            {
-                sum += numbers[i];
-            }
-            Consile.WriteLine(sum);
+            // System.Console.WriteLine("The sum of the even numbers : ");
+            // if(numbers[i] % 2 == 0)
+            // {
+            //     sum += numbers[i];
+            // }
+            // Consile.WriteLine(sum);
+
+            // int[] arr = new int[1];
+            // int i = 0;
+            // int sum  = 0;
+
+            // while(true)
+            // {
+            //     System.Console.WriteLine("Enter a number or press Q to quit : ");
+            //     string input = Console.ReadLine();
+                
+            //     if(input.ToUpper == "Q")
+            //     {
+            //         break;
+            //     }
+
+            //     int num = int.Parse(input);
+
+            //     if((num >= 0) && (num <= 9))
+            //     {
+            //         for(int i = 0; i < num.Length; i++)
+            //         {
+            //             Array.Resize(ref arr, arr.Length * 2);
+            //             sum += arr[i];
+            //         }
+            //     }
+
+            //     else
+            //     {
+            //         System.Console.WriteLine($"Sum : {sum}");
+            //     }
+
+            // }
+
+            // int[] arr = {1,2,3,4,5,6};
+            // int len = arr.Length();
+
+            // for (int i = 0; i < len/2; i++)
+            // {
+            //     int temp = arr[i];
+            //     arr[i] = arr[len - 1 - i];
+            //     arr[len - 1 - i] = temp;
+            // }
+            
+            // System.Console.WriteLine(string.Join(",",arr));
+
+
+            //------------------
+            // Console.WriteLine("Enter the number of elements in the array: ");
+            // int len = int.Parse(Console.ReadLine());
+
+            // int[] arr = new int[len];
+            // Console.WriteLine($"Enter {len} numbers: ");
+
+            // for (int i = 0; i < len; i++)
+            // {
+            //     arr[i] = int.Parse(Console.ReadLine());
+            // }
+
+            
+            // int mid = len/2;
+            
+            // if(len % 2 == 0)
+            // {
+            //     for (int i = 0; i < mid/2; i++)
+            //     {
+            //         int temp = arr[i];
+            //         arr[i] = arr[len - 1 - i];
+            //         arr[len - 1 - i] = temp;
+            //     }
+                
+            //     for (int i = 0; i < mid + (len-mid)/2; i++)
+            //     {
+            //         int temp = arr[i];
+            //         arr[i] = arr[len - 1 - (i-mid)];
+            //         arr[len - 1 - (i-mid)] = temp;
+            //     }
+                
+            // }
+            
+            // else
+            // {
+            //     for (int i = 0; i < mid/2; i++)
+            //     {
+            //         int temp = arr[i];
+            //         arr[i] = arr[len - 1 - i];
+            //         arr[len - 1 - i] = temp;
+            //     }
+                
+            //     for (int i = 0; i < mid + 1 + (len-mid)/2; i++)
+            //     {
+            //         int temp = arr[i];
+            //         arr[i] = arr[len - 1 - (i-mid)];
+            //         arr[len - 1 - (i-mid)] = temp;
+            //     }
+            // }
+        
+                
+            // System.Console.WriteLine(string.Join(",",arr));
+            //----------------------
+
+
+
+
+
+
+                
+        }
+
         }
     }
 }
