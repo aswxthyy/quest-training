2c8fd142ab3bb08389423595bd711a110b9411da
Aswathy Krishnan <aswathy@Aswathys-MacBook-Air.local>
2024-10-04 17:28:48 +0530

    day4commit

---
+++ Day4/Functions/Program.cs
@@ -0,0 +1,234 @@
+﻿internal class Program
+{
+/*
+    static int Add(int a, int b) => a + b;
+    static int Sub(int a, int b) => a - b;
+    static int Mul(int a, int b) => a * b;
+    static int Div(int a, int b) => a / b;
+ 
+    private static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        string op = Console.ReadLine();
+
+        switch(op)
+        {
+            case "+":
+            {
+                Add(x,y);
+                break;
+            }
+            case "-":
+            {
+                Sub(x,y);
+                break;
+            }
+            case "*":
+            {
+                Mul(x,y);
+                break;
+            }
+            case "/":
+            {
+                Div(x,y);
+                break;
+            }
+            default:
+            {
+                Console.WriteLine("Invalid Operator");
+                break;
+            }
+
+        }
+        
+    }
+*/
+
+//-------------------------------------------------
+
+/*
+    static void Names(string[] input)
+    {
+        string[] names = { "alpha", "beta", "gamma", "delta", "epsilon" };
+        bool found = false;
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (input[0] == names[i]) 
+            { 
+                Console.WriteLine($"The name {input[0]} is present");
+                found = true;
+                break;  
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Name not present");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a name : ");
+        string input = Console.ReadLine();
+        input = input.ToLower();  
+        Names(new string[] { input });
+    }
+
+*/
+
+//----------------------------------------
+/*   static void Names(string[] input)
+    {
+        string[] names = { "alpha", "beta", "gamma", "delta", "epsilon", "alice", "bob", "cathy", "david","ernie"};
+        bool found = false;
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (input[0] == names[i])
+            { 
+                Console.WriteLine($"The name {input[0]} is present");
+                found = true;
+                break;  
+            }
+            /*
+            if (names[i].StartsWith(inputLetter.ToString(), StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine(names[i]);
+                found = true;
+            }
+            
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Name not present");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a name : ");
+        string input = Console.ReadLine().ToLower().Trim(); 
+        Names(new string[] { input });
+  
+    }
+*/
+//--------------------------------
+   /* System.Console.WriteLine("Enter a sentence : ");
+    string sent = Console.ReadLine();
+
+    System.Console.WriteLine("Enter the text to be replaced : ");
+    string text = Console.ReadLine();
+
+    System.Console.WriteLine("Enter the text to be replaced with : ");
+    string replace = Console.ReadLine();
+
+    Console.WriteLine(sent.Replace(text,replace));
+
+*/
+//-------------------------------
+    private static void Main(string[] args)
+    {
+    /*
+        System.Console.Write("Enter a sentence : ");
+        string line = Console.ReadLine();
+
+        string[] words = line.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            int count = 0;
+            string word = words[i];
+
+            bool counted = false;
+
+            for(int j = 0; j < words.Length ; j++)
+            {
+                if (words[j] == word)
+                {
+                    counted = true;
+                }
+            }
+
+            if(counted == true)
+            {
+                for(int k = 0; k < words.Length ; k++)
+                {
+                    if (words[k] == word)
+                    {
+                        count++;
+                    }
+                }
+
+                Console.WriteLine($"'{word}' : {count}");
+            }
+
+        }
+    */
+
+
+    //foreach
+    /*
+       string text = "This is a long long long sentence";
+       string[] words = text.Split(' ');
+       string[] found = new string[words.Length];
+       int checker = 0;
+
+       foreach(var word in words)
+       {
+        if(found.Contains(words))
+            continue;
+
+        int count = 0;
+
+        foreach(var item in words)
+        {
+            if(item == word)
+                count++;
+        }
+
+        found[counter] = word;
+        counter++;
+
+        Console.WriteLine($"'{word}' : {count}");
+
+        }
+      
+        */
+
+
+        //----------------------------------
+
+        /*
+        System.Console.Write("Enter a word to check if it's a palindrome : ");
+        string word = Console.ReadLine();
+
+        string rev = new string(word.Reverse().ToArray());
+
+        if(rev == word)
+        {
+            System.Console.WriteLine("The string is a palindrome");
+        }
+
+        else
+        {
+            System.Console.WriteLine("Not a palindrome");
+        }
+
+        */
+
+        //-----------------------------------
+
+        
+
+
+    }
+
+
+    
+}
+
+
---
+++ Day4/StringOperations/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+        var res = "";
+        res = string.Join(", ","Hello", "World");
+        System.Console.WriteLine(res);
+        
+        var arr = new string[]{"Hi", "World"};
+        res = string.Join(", ",arr);
+        System.Console.WriteLine(res);
+
+        res = string.Format("{0} {1} {2} and {2}", "Hello", "World", "C#", "C++");
+        System.Console.WriteLine(res);
+
+        //String Builder
+        var sb = new StringBuilder();
+        sb.AppendLine("This is a string builder");
+        sb.AppendLine();
+        sb.AppendLine("Now there's a blank line");
+        res = sb.ToString();
+        System.Console.WriteLine(res);
+        
+
+        var names = new string[]{"alpha","beta","gamma","delta"};
+       
+        int index = 0;
+        foreach (var name in names)
+        {
+            sb.AppendFormat("{0} is {1} years old", name, index);
+            sb.AppendLine();
+            index++; 
+
+            System.Console.WriteLine(sb.ToString()); 
+        }    
+
+        //reverse each half of an array
+        
+        // int[] numbers = {1,2,3,4,5};
+        // int mid = numbers[(numbers.Length/2)+1];
+        // var firstHalf = numbers.Take(numbers.Length/2).Reverse().ToArray();
+        // var secHalf = numbers.Skip(numbers.Length/2 + 1).Reverse().ToArray();
+
+        // firstHalf.CopyTo(numbers,0);
+        // secHalf.CopyTo(numbers,numbers.Length/2 + 1);
+        // Console.WriteLine(string.Join(" ",numbers));
+        
+
+        int[] numbers = {1,2,3,4};
+        var firstHalf = numbers.Take(numbers.Length/2).Reverse().ToArray();
+        var secHalf = numbers.Skip(numbers.Length/2).Reverse().ToArray();
+
+        firstHalf.CopyTo(numbers,0);
+        secHalf.CopyTo(numbers,numbers.Length/2);
+        Console.WriteLine(string.Join(" ",numbers));
+        */
+
+
+        /*
+        Console.Write("Enter numbers or their names : ");
+        var input = Console.ReadLine();
+
+        var sb = new StringBuilder();
+
+        foreach (var i in input)
+        {
+            switch (i)
+            {
+                case '1':
+                {
+                    sb.Append("one");
+                    break;
+                }                
+                case '2':
+                {
+                    sb.Append("two");
+                    break;
+                }            
+                case '3':
+                {
+                    sb.Append("three");
+                    break;
+                }                   
+                case '4':
+                {
+                     sb.Append("four");
+                    break;
+                }                  
+                case '5':
+                {
+                    sb.Append("five");
+                    break;
+                }
+                default:
+                    sb.Append(i); 
+                    break;
+            }
+        }
+
+        Console.WriteLine(sb.ToString());
+
+        //--------------------------------------------
+        Console.Write("Enter a string : ");
+        var input = Console.ReadLine();
+
+        //Ternary Operator
+        //foreach(var i in text)
+        //{
+        //    char c = char.IsUpper(i) ? char.ToLower(i) : char.ToUpper(i);
+        //}
+
+        var sb = new StringBuilder();
+
+        foreach (var i in input)
+        {
+            char c = i; 
+            if ((i >= 'a') && (i <= 'z')) 
+            {
+                c = (char)(i - 32);
+            }
+            else if ((i >= 'A') && (i <= 'Z')) 
+            {
+                c = (char)(i + 32);
+            }
+            else
+                continue;
+
+            sb.Append(c); 
+        }
+
+        Console.WriteLine(sb.ToString());
+        */
+        //----------------------------------
+        
+        Console.Write("Enter a number : ");
+        var input = int.Parse(Console.ReadLine());
+
+        int num = 1;
+
+        for (int i = 1; i <= input; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("{0,4}", num++); 
+            }
+            Console.WriteLine();
+        }
+        
+
+        // Console.Write("Enter a number : ");
+        // var input = int.Parse(Console.ReadLine());
+        
+
+        // for (int i = 1; i <= input; i++)
+        // {
+        //     for (int j = 1; j <= i; j++)
+        //     {
+        //         Console.Write(j);
+        //     }
+        //     Console.WriteLine();
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
