20b2f779ba877da20254ba5e709cae715bee3327
Aswathy <63112848+aswxthyy@users.noreply.github.com>
2024-10-16 19:55:06 +0530

    day12commit

--- Day12/GetSet/Person.cs
+++ Day12/GetSet/Person.cs
@@ -1,3 +1,53 @@
+// namespace GetSet
+// {
+//     public class Student
+//     {
+//         public string Name { get; set; } 
+//         private int age;
+
+//         public int Age { 
+//             get => age; 
+//             set
+//             {
+//                 if((value > 0) && (value <50))
+//                 {
+//                     age = value;
+//                 }
+//             } }
+
+//     }
+// }
+/*
+{get; set;} is a 'property' we use in order to impose restrictions while retrieving or saving data
+it's possible with functions too but those aren't quite efficcient, hence this property. 
+also because some variables may require to be private for security reasons, some maybe only need to be Read Only.
+by using {get; set;} the variables are safely exposed to different classes
+mishel says it's good practice to declare ALL your data types in this way,
+and always declare your private variables in lower case 
+public int MyProperty { get; set; }
+
+just type 'prop' and press Tab the rest of it gets autofilled
+
+get() to retreive data
+set() to save the data
+
+in Line 5 there aren't any imposed restrictions on Name, we accept and print whatever data the user enters.
+in Line 8 for Age we impose restrictions on it, meaning the data is stored only if((value > 0) && (value <50))
+value is a keyword here, the user's input value is passed automatically.
+{ get; set; } is just expanded as 
+{
+    get 
+    {
+        whatever condition
+    };
+    set
+    {
+        whatever condition
+    };
+}
+
+*/
+
 namespace GetSet
 {
     public class Person
--- Day12/GetSet/Program.cs
+++ Day12/GetSet/Program.cs
@@ -1,4 +1,36 @@
-﻿namespace GetSet
+﻿// namespace GetSet
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             var s = new Student
+//             {
+//                 Name = "Alpha",
+//                 Age = 200
+//             };
+//             System.Console.WriteLine(s.Name);
+//             System.Console.WriteLine(s.Age);
+//         }
+//     }
+// }
+/*
+now if you take a look at the Program class, we create an object s of our class Student,
+enter values into it.
+now, since we have  set
+                    {
+                        if((value > 0) && (value <50))
+                        {
+                            age = value;
+                        }
+                    } 
+and set means to store the data, 
+if Age  = 200 here our program wont even store the value since it has to be > 0 and < 50
+now when s.Age is printed, it displays 0.
+try giving another number > 0 and < 50 suppose 25, and it'll print 25
+*/
+
+namespace GetSet
 {
     internal class Program
     {
